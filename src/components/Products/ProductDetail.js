49d5524817859bb8de152ec3887443220d6834f7
unknown <emmyobonyo@gmail.com>
2022-07-26 16:22:56 +0300

    Show items on the air tags and air pods

--- src/components/Products/ProductDetail.js
+++ src/components/Products/ProductDetail.js
@@ -45,56 +45,27 @@ class ProductDetail extends PureComponent {
               <div className="detail-page-details">
                 <h3 className="product-detail-brand">{data.product.brand}</h3>
                 <h3 className="product-detail-name">{data.product.name}</h3>
-                { data.product.category === 'clothes' && data.product.attributes.length > 0 &&
+                { data.product.attributes.length > 0 &&
                   <div>
                     { data.product.attributes.map((item) => (
                       <div key={nanoid()}>
                         <h4 style={{ marginBottom: 10, marginTop: 40 }}>{`${item.name.toUpperCase()}:`}</h4>
                         <div>
-                          {item.items.map((itemAttribute) => (
+                          { item.name == 'Color' && item.items.map((itemAttribute) => (
+                            <span className="swatches" key={nanoid()} style={{ backgroundColor: itemAttribute.value, }}></span>
+                          ))}
+                          { !(item.name == 'Color') && item.items.map((itemAttribute) => (
                             <span className="product-detail-items" key={nanoid()}>{itemAttribute.value}</span>
                           ))}
                         </div>
                       </div>
                     )) }
-                    <h4 style={{marginBottom: 10, marginTop: 30}}>PRICE</h4>
-                    <ChangeCurrencyLogic currency={currency} id={id}/>
-                    <button onClick={() => addToCart(data.product)} className="detail-page-button" style={{ marginTop: 20, marginBottom: 20}} type="buton">ADD TO CART</button>
-                    <Interweave content={data.product.description}/>
                   </div>
                 }
-                { data.product.category === 'tech' && data.product.attributes.length > 0 &&
-                  <div>
-                  { data.product.attributes.map((item) => (
-                    <div key={nanoid()}>
-                      { item.name === 'Color' &&
-                        <div>
-                          <h3 style={{ marginBottom: 10, marginTop: 40 }}>{`${item.name.toUpperCase()}:`}</h3>
-                          <div>
-                            {item.items.map((value) => (
-                              <span className="swatches" key={nanoid()} style={{ backgroundColor: value.value, }}></span>
-                            ))}
-                          </div>
-                        </div>
-                      }
-                      { item.name === 'Capacity' &&
-                        <div>
-                          <h3>{item.name}</h3>
-                          <div>
-                            {item.items.map((value) => (
-                              <span className="detail-page-capacity" key={nanoid()}>{value.value}</span>
-                            ))}
-                          </div>
-                        </div>
-                      }
-                    </div>
-                  )) }
-                  <h4>PRICE</h4>
-                  <ChangeCurrencyLogic currency={currency} id={id}/>
-                  <button onClick={() => addToCart(data.product)} className="detail-page-button" style={{ marginTop: 20, marginBottom: 20}}>ADD TO CART</button>
-                  <Interweave content={data.product.description}/>
-                </div>
-                }
+                <h4 style={{marginBottom: 10, marginTop: 30}}>PRICE</h4>
+                <ChangeCurrencyLogic currency={currency} id={id}/>
+                <button onClick={() => addToCart(data.product)} className="detail-page-button" style={{ marginTop: 20, marginBottom: 20}} type="buton">ADD TO CART</button>
+                <Interweave content={data.product.description}/>
               </div>
             </div>
           )
