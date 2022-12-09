01610ddb6aec840eb1cbb81fad543df3d6ecaead
Emmanuel Obonyo <emmyobonyo@gmail.com>
2022-12-09 11:22:16 +0300

    Assign state to keys and values

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,21 +1,24 @@
-import { PureComponent } from 'react';
-import { withRouter } from './withRouter';
-import { useParams } from 'react-router-dom';
-import { Query } from '@apollo/client/react/components';
-import GET_PRODUCTS from '../../graphql/getProducts';
-import ProductItem from './ProductItem';
-import Filter from './Filter';
+import { PureComponent } from 'react'
+import { withRouter } from './withRouter'
+import { useParams } from 'react-router-dom'
+import { Query } from '@apollo/client/react/components'
+import GET_PRODUCTS from '../../graphql/getProducts'
+import ProductItem from './ProductItem'
+import Filter from './Filter'
 import './Product.css'
 
 class Product extends PureComponent {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       stateChanged: false,
+      name: null,
+      nameValue: null,
     }
   }
 
   render() {
+    // console.log(this.state.nameVlaue)
     const changeProductState = (e) => {
       const value = e.target.value
       const key = e.target.parentElement.firstChild.innerText
@@ -23,12 +26,18 @@ class Product extends PureComponent {
         this.props.navigate(`/all/?${key}=${value}`)
       } else {
         this.props.navigate(`/${category}/?${key}=${value}`)
+        this.setState({ name: key, nameValue: value })
       }
     }
-    const { category } = this.props.params;
-    const { homepage, currency, addToCart, closeCurrencyOverlay, disabled } = this.props;
+    console.log(this.state)
+    const { category } = this.props.params
+    const { homepage, currency, addToCart, closeCurrencyOverlay, disabled } =
+      this.props
     return (
-      <div className='products' onClick={()=>closeCurrencyOverlay()}>
+      <div
+        className='products'
+        onClick={() => closeCurrencyOverlay()}
+      >
         <h1 className='category'>{!homepage ? category : homepage}</h1>
         <div className='product-listing-page'>
           <div className='product-listing-filter'>
@@ -36,26 +45,37 @@ class Product extends PureComponent {
           </div>
           <div className='product-items'>
             <Query
-              key="yes"
+              key='yes'
               query={GET_PRODUCTS}
-              variables={{ input: { title: `${!category ? homepage : category}` } }}
-              fetchPolicy="network-only"
+              variables={{
+                input: { title: `${!category ? homepage : category}` },
+              }}
+              fetchPolicy='network-only'
             >
               {({ loading, data }) => {
-                if (loading) return null;
+                if (loading) return null
                 console.log(data)
-                return <ProductItem data={data} disabled={disabled} addToCart={addToCart} currency={currency} />
+                return (
+                  <ProductItem
+                    data={data}
+                    disabled={disabled}
+                    addToCart={addToCart}
+                    currency={currency}
+                    name={this.state.name}
+                    value={this.state.nameValue}
+                  />
+                )
               }}
             </Query>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 export default withRouter((props) => (
   <Product
     {...props}
     params={useParams()}
-  />)
-);
+  />
+))
