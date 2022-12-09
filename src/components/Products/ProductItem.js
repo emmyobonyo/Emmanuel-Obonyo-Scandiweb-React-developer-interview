5faf52d701d6c2b94d4d856af6e565e5c654d7f4
Emmanuel Obonyo <emmyobonyo@gmail.com>
2022-12-09 14:34:22 +0300

    Add a consitional to render different things

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -33,39 +33,11 @@ class ProductItem extends PureComponent {
   render() {
     const { disabled, addToCart, currency, name, value } = this.props
     console.log(name, value)
-    return (
-      name == null &&
-      value == null &&
-      this.state.data.map((product) => (
-        <div key={nanoid()}>
-          {!product.inStock ? (
-            <div>
-              <ProductComponent
-                product={product}
-                disabled={disabled}
-                currency={currency}
-                showAddToCartButton={this.showAddToCartButton}
-                hideAddToCartButton={this.hideAddToCartButton}
-              />
-              {product.attributes.length === 0 &&
-                this.state.onHover &&
-                product.hover &&
-                product.inStock && (
-                  <img
-                    src={Common}
-                    alt='add-to-cart'
-                    onClick={() => addToCart(product)}
-                    className='add-to-cart-button'
-                  />
-                )}
-            </div>
-          ) : product.attributes.length === 0 ? (
-            <div
-              className='add-to-cart-product-div'
-              onMouseEnter={() => this.showAddToCartButton(product.id)}
-              onMouseLeave={() => this.hideAddToCartButton(product.id)}
-            >
-              <Link to={`/product/${product.id}`}>
+    return name == null && value == null
+      ? this.state.data.map((product) => (
+          <div key={nanoid()}>
+            {!product.inStock ? (
+              <div>
                 <ProductComponent
                   product={product}
                   disabled={disabled}
@@ -73,34 +45,60 @@ class ProductItem extends PureComponent {
                   showAddToCartButton={this.showAddToCartButton}
                   hideAddToCartButton={this.hideAddToCartButton}
                 />
-              </Link>
-              {product.attributes.length === 0 &&
-                product.hover &&
-                product.inStock && (
-                  <img
-                    src={Common}
-                    alt='add-to-cart'
-                    onClick={() => addToCart(product)}
-                    className='add-to-cart-button'
+                {product.attributes.length === 0 &&
+                  this.state.onHover &&
+                  product.hover &&
+                  product.inStock && (
+                    <img
+                      src={Common}
+                      alt='add-to-cart'
+                      onClick={() => addToCart(product)}
+                      className='add-to-cart-button'
+                    />
+                  )}
+              </div>
+            ) : product.attributes.length === 0 ? (
+              <div
+                className='add-to-cart-product-div'
+                onMouseEnter={() => this.showAddToCartButton(product.id)}
+                onMouseLeave={() => this.hideAddToCartButton(product.id)}
+              >
+                <Link to={`/product/${product.id}`}>
+                  <ProductComponent
+                    product={product}
+                    disabled={disabled}
+                    currency={currency}
+                    showAddToCartButton={this.showAddToCartButton}
+                    hideAddToCartButton={this.hideAddToCartButton}
                   />
-                )}
-            </div>
-          ) : (
-            <div className='add-to-cart-product-div'>
-              <Link to={`/product/${product.id}`}>
-                <ProductComponent
-                  product={product}
-                  disabled={disabled}
-                  currency={currency}
-                  showAddToCartButton={this.showAddToCartButton}
-                  hideAddToCartButton={this.hideAddToCartButton}
-                />
-              </Link>
-            </div>
-          )}
-        </div>
-      ))
-    )
+                </Link>
+                {product.attributes.length === 0 &&
+                  product.hover &&
+                  product.inStock && (
+                    <img
+                      src={Common}
+                      alt='add-to-cart'
+                      onClick={() => addToCart(product)}
+                      className='add-to-cart-button'
+                    />
+                  )}
+              </div>
+            ) : (
+              <div className='add-to-cart-product-div'>
+                <Link to={`/product/${product.id}`}>
+                  <ProductComponent
+                    product={product}
+                    disabled={disabled}
+                    currency={currency}
+                    showAddToCartButton={this.showAddToCartButton}
+                    hideAddToCartButton={this.hideAddToCartButton}
+                  />
+                </Link>
+              </div>
+            )}
+          </div>
+        ))
+      : console.log(product.attributes)
   }
 }
 
